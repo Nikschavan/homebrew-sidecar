7dc57772feb52fad259622ceb7749774e51f6216
Nikhil <nikhil.chavan@automattic.com>
2026-01-10 23:22:35 +0530

    Update sidecar to v0.0.2

--- Formula/sidecar.rb
+++ Formula/sidecar.rb
@@ -1,12 +1,12 @@
 class Sidecar < Formula
   desc "Permission management sidecar for Claude Code CLI"
   homepage "https://github.com/Nikschavan/Sidecar"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   on_macos do
     url "https://github.com/Nikschavan/Sidecar/releases/download/v#{version}/sidecar"
-    sha256 "66e5fae5b877ddea47d1fc55b6e37349acada3c126b96367900c6d7929c27fce"
+    sha256 "af4d6f4ffa3734873a9ae30cc16d20b1b2861f7d9266eff33e0b4f307ec99e2c"
   end
 
   def install
